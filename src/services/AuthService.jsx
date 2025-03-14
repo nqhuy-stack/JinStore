f7459ba4a178f7822a7c38a7812dbcf9bbbde106
nqhit <nguyenquochuy812003@gmail.com>
2025-03-14 18:05:34 +0700

    .

--- src/services/AuthService.jsx
+++ src/services/AuthService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = import.meta.env.VITE_API_URL;
-const API_URL = 'https://jinstore-api.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://jinstore-api.onrender.com';
 
 // Đăng ký tài khoản
 export const registerUser = async (userData) => {
