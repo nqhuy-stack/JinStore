ea9b76a13c4a4f46ce3582ca1238e07a975bc9ab
NguyenSNguyen <nsnguyen.723.qbvn@gmail.com>
2025-04-10 04:08:00 +0700

    sff

--- src/components/common/Breadcrumb.jsx
+++ src/components/common/Breadcrumb.jsx
@@ -3,21 +3,50 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
+// Thêm inline styles
+const customStyles = {
+  item: {
+    fontSize: '1.5rem',
+    fontWeight: '700',
+  },
+  separator: {
+    fontSize: '1.2rem',
+    margin: '0 0.5rem',
+  },
+  icon: {
+    fontSize: '1.32rem',
+    marginRight: '0.5rem',
+  },
+};
+
 const Breadcrumb = ({ items }) => {
   return (
     <nav className="breadcrumb" aria-label="breadcrumb">
-      <div className="breadcrumb__container">
-        <div className="breadcrumb__item breadcrumb__item--home">
-          <Link to="/">
-            <FontAwesomeIcon icon={faHome} className="breadcrumb__icon" />
+      <div className="breadcrumb__container" style={{ marginLeft: 0, paddingLeft: 0 }}>
+        <div
+          className="breadcrumb__item breadcrumb__item--home"
+          style={{ ...customStyles.item, marginLeft: 0, paddingLeft: 0 }}
+        >
+          <Link to="/" style={customStyles.item}>
+            <FontAwesomeIcon icon={faHome} className="breadcrumb__icon" style={customStyles.icon} />
             <span>HOME</span>
           </Link>
         </div>
 
         {items.map((item, index) => (
-          <div key={index} className={`breadcrumb__item ${!item.link ? 'breadcrumb__item--active' : ''}`}>
-            <FontAwesomeIcon icon={faChevronRight} className="breadcrumb__separator" />
-            {item.link ? <Link to={item.link}>{item.text.toUpperCase()}</Link> : <span>{item.text.toUpperCase()}</span>}
+          <div
+            key={index}
+            className={`breadcrumb__item ${!item.link ? 'breadcrumb__item--active' : ''}`}
+            style={customStyles.item}
+          >
+            <FontAwesomeIcon icon={faChevronRight} className="breadcrumb__separator" style={customStyles.separator} />
+            {item.link ? (
+              <Link to={item.link} style={customStyles.item}>
+                {item.text.toUpperCase()}
+              </Link>
+            ) : (
+              <span>{item.text.toUpperCase()}</span>
+            )}
           </div>
         ))}
       </div>
