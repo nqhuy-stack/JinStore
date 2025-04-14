72d4f0c5f24e7727f68538e8616dc8c06890a29e
nqhit <nguyenquochuy812003@gmail.com>
2025-04-14 19:40:19 +0700

    chore: tối ưu đường link url google login

--- src/components/common/SocialAccount.jsx
+++ src/components/common/SocialAccount.jsx
@@ -2,6 +2,7 @@ import iconGoogleLogin from '@assets/icons/icongooglelogin.svg';
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const APP_URL = import.meta.env.VITE_APP_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const LINK_GET_TOKEN =
   `https://accounts.google.com/o/oauth2/v2/auth?` +
@@ -9,7 +10,7 @@ const LINK_GET_TOKEN =
   `response_type=token&` +
   `redirect_uri=${APP_URL}&` +
   `client_id=${GOOGLE_CLIENT_ID}`;
-const GOOGLE_LOGIN_URL = 'http://localhost:1000/api/auth/google';
+const GOOGLE_LOGIN_URL = `${API_URL}/auth/google`;
 
 function SocialAccount() {
   const googleAuth = () => {
