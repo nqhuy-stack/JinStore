daeafa26458a0e2a2cd561748f12305020ad17c2
nqhit <nguyenquochuy812003@gmail.com>
2025-07-19 09:57:08 +0700

    fix: transports của setting socket

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,10 @@
+// socket.js - thay đổi thành default export
 import { io } from 'socket.io-client';
 
 const API_URL = import.meta.env.VITE_API_URL_V;
 
 const socket = io(API_URL, {
+  transports: ['polling', 'websocket'],
   withCredentials: true,
 });
 
